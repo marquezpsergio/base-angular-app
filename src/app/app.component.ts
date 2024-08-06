8bca3918b73fa3951bed6dd4c2d069b810fb98ec
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-06 16:25:06 +0200

    feat: base angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task_management_front';
+  title = 'base-angular18-app';
 }
